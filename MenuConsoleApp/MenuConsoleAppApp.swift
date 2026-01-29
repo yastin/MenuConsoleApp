3931ba4c9cebdb88c2e2eeef4b9725fbd4040a1b
Andrew Yastin <yastin@ukr.net>
2026-01-29 08:24:04 +0200

    Initial Commit

---
+++ MenuConsoleApp/MenuConsoleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuConsoleAppApp.swift
+//  MenuConsoleApp
+//
+//  Created by Andrew Yastin on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MenuConsoleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
